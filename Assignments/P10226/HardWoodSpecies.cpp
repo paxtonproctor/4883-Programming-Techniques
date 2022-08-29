4aeae8d95da770bdfa05b60a11336980ce4aa023
Paxton Proctor <61135201+paxtonproctor@users.noreply.github.com>
2022-08-29 14:40:26 -0500

    Update HardWoodSpecies.cpp

--- Assignments/P10226/HardWoodSpecies.cpp
+++ Assignments/P10226/HardWoodSpecies.cpp
@@ -2,13 +2,12 @@
 // 4883 Programming Techniques
 // 8/26/2022
 
-#include <iomanip>
 #include <iostream>
 #include <map>
 #include <string>
 
+// print integer new line new line
 #define SCD(t) scanf("%d\n\n", &t)
-#define endl "\n"
 
 //  ./main < input1
 
@@ -19,43 +18,50 @@ using namespace std;
 int main() {
   // map counts the amount of times a cetain string pops up
   map<string, int> treeCount;
+  map<string, int>::iterator itr;
 
+  // declarations
   int T;
-  double count;
   string tree;
 
-  // supposed to be faster
+  // cin > T;
   SCD(T);
 
-  // clear the spaces
+  // reads the 1 and the space
   getline(cin, tree);
-  
-  // eof
-  while (T--) {
+  getline(cin, tree);
 
-    getline(cin, tree);
+  while (T > 0) {
     
-    if (tree.size()) {
-      count++;
-      // if string is the same insert it into map
-      if (treeCount.find(tree) == treeCount.end()) {
-        treeCount.insert(pair<string, int>(tree, 0));
+    int count = 0;
+
+    while (getline(cin, tree)) {
+      // if a space is hit break the while loop
+      if (tree == "") {
+        break;
       }
 
-      // there might be a faster way
+      // count the iterator
+      count++;
+
+      // count the number of each tree
       treeCount[tree]++;
-      
-    } else {
-      for (auto itr = treeCount.cbegin(); itr != treeCount.cend(); ++itr) {
-        // %s for string followed by %denote.fourdecimal places long doouble and new line
-        printf("%s %.4lf\n", (*itr).first.data(),
-          (double) (*itr).second / (double) count * 100.0);
-      }
     }
     
-    printf("\n");
-    treeCount.clear();
-    count = 0;
+    // from beginning of file to end will print the trees and there avg
+    for (itr = treeCount.begin(); itr != treeCount.end(); itr++) {
+      // print string %s, % is denote, 4 decimal places. Long Double, new line.
+      printf("%s %.4lf\n", (*itr).first.data(),
+             (double)(*itr).second / (double)count * 100.0);
+    }
+
+    // decrement
+    T--;
+
+    if (T > 0) {
+      printf("\n");
+      treeCount.clear();
+    }
   }
   return 0;
 }
