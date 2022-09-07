71c4eaf2627adc223e261ca1701ce93ac73ca06b
Paxton Proctor <61135201+paxtonproctor@users.noreply.github.com>
2022-09-07 14:22:26 -0500

    Create Back_To_High_School_Physics.cpp

---
+++ Assignments/Easy Breazy/P10071/Back_To_High_School_Physics.cpp
@@ -0,0 +1,21 @@
+// Paxton Proctor
+// Programming Techniques 4883
+// 9/7/2022
+
+#include <stdio.h>
+
+// scanf int macro
+#define SCD(a,b) scanf("%d %d", &a, &b)
+
+// printf  int macro
+#define PFD(a,b) printf("%d\n", 2 * a * b);
+
+// while  eof macro
+#define WHILE(a,b) while (SCD(a,b) != EOF)
+
+int main() {
+  int a,b;
+  WHILE(a,b){
+    PFD(a,b);
+  }
+}
